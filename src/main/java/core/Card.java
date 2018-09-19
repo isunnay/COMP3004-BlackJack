b531aa4596ad66a02918cab288efc3f6741cd549
Elvin Sunny <elvin.sunny.07@gmail.com>
2018-09-19 01:37:32 -0400

    Made minor changes to Card and CardTest

--- src/main/java/core/Card.java
+++ src/main/java/core/Card.java
@@ -1,23 +1,55 @@
 package core;
 
 public class Card{
-	private String suit;
-	private String rank;
+	private int suit;
+	private int rank;
 	
-	public Card(String suit, String rank) {
+	private static final String[] suits = {"C", "D", "H", "S"};
+    private static final String[] ranks = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+	
+	public Card(int suit, int rank) {
 		this.suit = suit;
 		this.rank = rank;
 		
 	}
 	
 	public String getCardSuit() {
-		return this.suit;
+		return suits[this.suit];
 		
 	}
 	
 	public String getCardRank() {
-		return this.rank;
+		return ranks[this.rank];
+	}
+	
+	
+	public String getCardName() {
+		String suitForName = String.valueOf(suit);
+		String rankForName = String.valueOf(rank);
+		return suitForName + rankForName;
+	}
+	
+	public int getValueOfCard(){
+		//returns the value of the Card
+
+		if (this.rank==0){
+			return 11;
+		}
+
+		else if (this.rank < 10){
+			return rank + 1;
+		}
+		else if(this.rank >=10){
+			return 10;
+		}
+		else{
+			return 0;
+		}
+	}
+	
+	public String toString(int suit, int rank) {
+		return suits[suit] + ranks[rank];
 		
 	}
-
+	
 }
--- src/test/java/core/CardTest.java
+++ src/test/java/core/CardTest.java
@@ -5,38 +5,57 @@ import junit.framework.TestCase;
 public class CardTest extends TestCase{
 	
 	public void testGetSuit() {
-		System.out.println("Testing Card Suit");
-		Card card = new Card("H", "3");
-		assertEquals("H", card.getCardSuit());
-		
-		card = new Card("S", "3");
-		assertEquals("S", card.getCardSuit());
-		
-		card = new Card("C", "3");
+		Card card = new Card(0, 3);
 		assertEquals("C", card.getCardSuit());
 		
-		card = new Card("D", "3");
+		card = new Card(1, 3);
 		assertEquals("D", card.getCardSuit());
+		
+		card = new Card(2, 3);
+		assertEquals("H", card.getCardSuit());
+		
+		card = new Card(3, 3);
+		assertEquals("S", card.getCardSuit());
 		}
 	
 	public void testGetRank() {
-		for(int i=3; i<=10;i++) {
-			Card card = new Card("H",String.valueOf(i));
-			assertEquals(String.valueOf(i),card.getCardRank());
+		for(int i=1; i<10;i++) {
+			Card card = new Card(2,i);
+			assertEquals(String.valueOf(i+1),card.getCardRank());
 	}
 		
-		Card card = new Card("H","J");
+		Card card = new Card(2,10);
 		assertEquals("J",card.getCardRank());
 		
-		card = new Card("H","Q");
+		card = new Card(2,11);
 		assertEquals("Q",card.getCardRank());
 		
-		card = new Card("H","K");
+		card = new Card(2,12);
 		assertEquals("K",card.getCardRank());
 		
-		card = new Card("H","A");
+		card = new Card(2,0);
 		assertEquals("A",card.getCardRank());
-		
 	}
+	
+	public void testGetValueOfCard() {
+		for(int i=1;i<10;i++) {
+			Card card = new Card(2,i);
+			assertEquals(i+1,card.getValueOfCard());
+		}
+		
+		Card card = new Card(2,10);
+		assertEquals(10,card.getValueOfCard());
+		
+		card = new Card(2,11);
+		assertEquals(10,card.getValueOfCard());
+		
+		card = new Card(2,12);
+		assertEquals(10,card.getValueOfCard());
+		
+		
+		card = new Card(2,0);
+		assertEquals(11,card.getValueOfCard());
+	}
+	
 	
 }
